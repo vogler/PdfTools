71e5a6335a926f0b1e399bce44a04b93753ece56
vogler <ralf.vogler@gmail.com>
2012-11-20 12:33:06 +0100

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+PdfTools
+========
+
+compactify.py
+-------------
+
+Usage:
+
+    python compactify.py lecture1.pdf lecture2.pdf
+
+Compacts the input pdfs for printing by removing superflous animated pages.
---
+++ compactify.py
@@ -0,0 +1,38 @@
+from pyPdf import PdfFileWriter, PdfFileReader
+import sys, os
+
+def compact(filein):
+	output = PdfFileWriter()
+	input1 = PdfFileReader(file(filein, "rb"))
+	# print the title of the document
+	print "title = %s" % (input1.getDocumentInfo().title)
+	n = input1.getNumPages()
+	# loop over pages
+	for i in range(1, n):
+		curr = input1.getPage(i)
+		prev = input1.getPage(i-1)
+		currTxt = curr.extractText()[:-3]
+		prevTxt = prev.extractText()[:-3]
+		if currTxt.find(prevTxt) == 0: # substring
+			pass # the current page is an extension to the previous one -> continue
+		else:
+			output.addPage(prev) # current page is something new -> save latest old one
+	output.addPage(input1.getPage(n-1)) # add last page
+	# write output
+	fileout = "%s-compact%s" % os.path.splitext(filein)
+	outputStream = file(fileout, "wb")
+	output.write(outputStream)
+	outputStream.close()
+	# print some information
+	n2 = output.getNumPages()
+	print "%s has %s pages." % (filein, n)
+	print "%s has %s pages." % (fileout, n2)
+	print "-> removed %s pages" % (n-n2)
+
+
+if len(sys.argv) < 2:
+	print "Usage: %s <inputfile(s)>" % sys.argv[0]
+	sys.exit(2)
+
+for filein in sys.argv[1:]:
+	compact(filein)
